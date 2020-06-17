198191ca3e0a2f80925bf9e7f5d8be319d89ec79
Infarh <shmachilin@yandex.ru>
2020-06-17 14:33:14 +0300

    Заготовка тестового проекта с интерфейсом пользвоателя

---
+++ WpfTest/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MathCore.WPF.ViewModels;
+
+namespace WpfTest.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        #region Title : string - Заголовок окна
+
+        /// <summary>Заголовок окна</summary>
+        private string _Title = "Заголовок главного окна";
+
+        /// <summary>Заголовок окна</summary>
+        public string Title { get => _Title; set => Set(ref _Title, value); }
+
+        #endregion
+    }
+}
