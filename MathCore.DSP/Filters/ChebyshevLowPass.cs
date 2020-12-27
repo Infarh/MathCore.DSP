d4fb54629843be0cda62da09a4041dfd4460959c
Infarh <shmachilin@yandex.ru>
2020-12-27 21:36:16 +0300

    Модульный тест аналитического расчёта фильтра Чебышева первого рода

--- MathCore.DSP/Filters/ChebyshevLowPass.cs
+++ MathCore.DSP/Filters/ChebyshevLowPass.cs
@@ -1,11 +1,156 @@
-﻿using MathCore.Annotations;
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+using static MathCore.Polynom.Array;
 
 namespace MathCore.DSP.Filters
 {
     public class ChebyshevLowPass : ChebyshevFilter
     {
-        public ChebyshevLowPass([NotNull] double[] B, [NotNull] double[] A) : base(B, A)
+        private static double arsh(double x) => Math.Log(x + Math.Sqrt(x * x + 1));
+        private static double arch(double x) => Math.Log(x + Math.Sqrt(x * x - 1));
+
+        [SuppressMessage("ReSharper", "InconsistentNaming")]
+        public enum ChebyshevType : byte
         {
+            /// <summary>Фильтр Чебышева первого рода - основной фильтр, пропускающий нижнюю полосу частот</summary>
+            I,
+            /// <summary>Фильтр Чебышева второго рода, подавляющий верхнюю область частот (выше fp)</summary>
+            II,
+            /// <summary>Фильтр Чебышева второго рода c коррекцией частотного диапазона, подавляющий верхнюю область частот (выше fs)</summary>
+            IICorrected
         }
+
+        private static Complex[] GetAnalogPolesI(int N, double EpsP)
+        {
+            var r = N % 2;                              // Нечётность порядка фильтра
+            var dth = Math.PI / N;                      // Угловой шаг между полюсами
+            var beta = arsh(1 / EpsP) / N;
+            var sh = Math.Sinh(beta);
+            var ch = Math.Cosh(beta);
+            var poles = new Complex[N];                 // Массив полюсов фильтра
+            if (r != 0) poles[0] = -sh;    // Если порядок фильтра нечётный, то первым добавляем центральный полюс
+            for (var i = r; i < poles.Length; i += 2)   // Расчёт полюсов
+            {
+                var th = dth * (i + 1 - r - 0.5);
+
+                var sin = Math.Sin(th);
+                var cos = Math.Cos(th);
+                poles[i] = new Complex(-sh * sin, ch * cos);
+                poles[i + 1] = poles[i].ComplexConjugate;
+            }
+            
+            return poles;
+        }
+
+        private static (Complex[] Zeros, Complex[] Poles) GetAnalogPolesII(int N, double EpsP)
+        {
+            var r = N % 2;                              // Нечётность порядка фильтра
+            var L = (N - r) / 2;                        // Число пар нулей
+            var dth = Math.PI / N;                      // Угловой шаг между полюсами
+            var beta = arsh(EpsP) / N;
+            var shb = Math.Sinh(beta);
+            var chb = Math.Cosh(beta);
+           
+            var poles = new Complex[N];                 // Массив полюсов фильтра
+            if (r != 0) poles[0] = -1 / shb;            // Если порядок фильтра нечётный, то первым добавляем центральный полюс
+            for (var i = r; i < poles.Length; i += 2)   // Расчёт полюсов
+            {
+                var th = dth * (i + 1 - r - 0.5);
+                var sin = Math.Sin(th);
+                var cos = Math.Cos(th);
+                var norm = 1 / (sin * sin * shb * shb + cos * cos * chb * chb);
+                poles[i] = new Complex(-shb * sin * norm, chb * cos * norm);
+                poles[i + 1] = poles[i].ComplexConjugate;
+            }
+
+            var zeros = new Complex[L * 2];
+            for (var n = 1; n <= L; n++)
+            {
+                var th = dth * (n - 0.5);
+                zeros[2 * n - 2] = new Complex(0, 1 / Math.Cos(th));
+                zeros[2 * n - 1] = zeros[2 * n - 2].ComplexConjugate;
+            }
+
+            return (poles, zeros);
+        }
+
+        private static (int N, double EpsP, double Wp, double kW) GetProperties(double fp, double fs, double dt, double Gp, double Gs)
+        {
+            if (!(fp < fs)) throw new InvalidOperationException("Частота пропускания должна быть меньше частоты подавления");
+            if (!(fp < 1 / (2 * dt))) throw new InvalidOperationException();
+
+            var Rp = -Gp.In_dB();
+            var Rs = -Gs.In_dB();
+
+            var eps_p = (10d.Pow(Rp / 10) - 1).Sqrt();
+            var eps_s = (10d.Pow(Rs / 10) - 1).Sqrt();
+
+            var Fp = ToAnalogFrequency(fp, dt);  // Частота пропускания аналогового прототипа
+            var Fs = ToAnalogFrequency(fs, dt);  // Частота подавления аналогового пропита
+
+            var Wp = Consts.pi2 * Fp;
+
+            var k_eps = eps_s / eps_p;
+            var k_W = Fs / Fp;
+
+            var N = (int)Math.Ceiling(arch(k_eps) / arch(k_W)); // Порядок фильтра
+
+            return (N, eps_p, Wp, k_W);
+        }
+
+        private static (double[] A, double[] B) InitializeI(double fp, double fs, double dt, double Gp, double Gs)
+        {
+            var properties = GetProperties(fp, fs, dt, Gp, Gs);
+            var poles = GetAnalogPolesI(properties.N, properties.EpsP);
+            var translated_poles = ToZArray(poles, dt, properties.Wp);
+
+            return (Array.Empty<double>(), Array.Empty<double>());
+        }
+
+        private static (double[] A, double[] B) InitializeII(double fp, double fs, double dt, double Gp, double Gs)
+        {
+            var properties = GetProperties(fp, fs, dt, Gp, Gs);
+            var polynom = GetAnalogPolesII(properties.N, properties.EpsP);
+            var z_poles = ToZArray(polynom.Poles, dt, properties.Wp);
+            var z_zeros = ToZArray(polynom.Zeros, dt, properties.Wp);
+
+            //var B = GetCoefficientsInverted(z_zeros).ToArray(b => b * G_norm).ToRe();
+            //var A = GetCoefficientsInverted(z_poles).ToRe();
+
+            return (Array.Empty<double>(), Array.Empty<double>());
+        }
+
+        private static (double[] A, double[] B) InitializeIICorrected(double fp, double fs, double dt, double Gp, double Gs)
+        {
+            var properties = GetProperties(fp, fs, dt, Gp, Gs);
+            var polynom = GetAnalogPolesII(properties.N, properties.EpsP);
+            var translated_poles = ToZArray(polynom.Poles, dt, properties.Wp / properties.kW);
+            var translated_zeros = ToZArray(polynom.Zeros, dt, properties.Wp / properties.kW);
+
+            return (Array.Empty<double>(), Array.Empty<double>());
+        }
+
+        public ChebyshevType FilterType { get; }
+
+        /// <summary>Инициализация нового фильтра Чебышева нижних частот</summary>
+        /// <param name="fp">Частота пропускания</param>
+        /// <param name="fs">Частота заграждения</param>
+        /// <param name="dt">Период дискретизации</param>
+        /// <param name="Gp">Затухание в полосе пропускания (0.891250938 = -1 дБ)</param>
+        /// <param name="Gs">Затухание в полосе заграждения (0.005623413 = -45 дБ)</param>
+        /// <param name="Type">Тип (род) фильтра чебышева</param>
+        public ChebyshevLowPass(double fp, double fs, double dt, double Gp = 0.891250938, double Gs = 0.005623413, ChebyshevType Type = ChebyshevType.I)
+            : this(Type switch
+            {
+                ChebyshevType.I => InitializeI(fp, fs, dt, Gp, Gs),
+                ChebyshevType.II => InitializeII(fp, fs, dt, Gp, Gs),
+                ChebyshevType.IICorrected => InitializeIICorrected(fp, fs, dt, Gp, Gs),
+                _ => throw new InvalidEnumArgumentException(nameof(Type), (int)Type, typeof(ChebyshevType))
+            }) => FilterType = Type;
+
+        private ChebyshevLowPass((double[] A, double[] B) config) : base(config.B, config.A) { }
     }
 }
---
+++ Tests/MathCore.DSP.Tests/Filters/ChecyshevLowPass.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Linq;
+
+using MathCore.DSP.Filters;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+// ReSharper disable InconsistentNaming
+
+namespace MathCore.DSP.Tests.Filters
+{
+    [TestClass]
+    public class ChecyshevLowPass : UnitTest
+    {
+        private static double arsh(double x) => Math.Log(x + Math.Sqrt(x * x + 1));
+        private static double arch(double x) => Math.Log(x + Math.Sqrt(x * x - 1));
+
+        [TestMethod]
+        public void TypeI_Creation()
+        {
+            const double pi2 = 2 * Math.PI;
+
+            const double fd = 5000;             // Частота дискретизации
+            const double dt = 1 / fd;           // Период дискретизации
+
+            const double fp = 1.0 * fd / pi2;   // Граничная частота конца интервала пропускания
+            const double fs = 1.6 * fd / pi2;   // Граничная частота начала интервала подавления
+
+            Assert.IsTrue(fp < fs);
+            Assert.IsTrue(fp < fd / 2);
+
+            //const double wp = Consts.pi2 * fp * dt; // 0.628318530717959 рад/с
+            //const double ws = Consts.pi2 * fs * dt; // 1.884955592153876 рад/с
+
+            const double Rp = 1.5;  // Неравномерность в полосе пропускания (дБ)
+            const double Rs = 35; // Неравномерность в полосе пропускания (дБ)
+
+            var eps_p = (10d.Pow(Rp / 10) - 1).Sqrt();
+            var eps_s = (10d.Pow(Rs / 10) - 1).Sqrt();
+
+            Assert.That.Value(eps_p).IsEqual(0.6422908567173865);
+            Assert.That.Value(eps_s).IsEqual(56.225240418946896);
+
+            var Gp = (-Rp).From_dB();
+            var Gs = (-Rs).From_dB();
+
+            Assert.That.Value(Gp).IsEqual(0.84139514164519513);
+            Assert.That.Value(Gs).IsEqual(0.017782794100389229);
+
+            Assert.That.Value(Gp.In_dB()).IsEqual(-1.5, 4.67e-15);
+            Assert.That.Value(Gs.In_dB()).IsEqual(-35);
+
+            var Fp = DigitalFilter.ToAnalogFrequency(fp, dt);  // Частота пропускания аналогового прототипа
+            var Fs = DigitalFilter.ToAnalogFrequency(fs, dt);  // Частота подавления аналогового прототипа
+
+            Assert.That.Value(Fp).IsEqual(869.46741682049208);
+            Assert.That.Value(Fs).IsEqual(1638.7206595257194);
+
+            var Wp = Consts.pi2 * Fp;
+            var Ws = Consts.pi2 * Fs;
+
+            var k_eps = eps_s / eps_p;
+            var k_W = Ws / Wp;
+
+            Assert.That.Value(k_eps).IsEqual(87.5385969314623);
+            Assert.That.Value(k_W).IsEqual(1.8847407364824178);
+
+            var N = (int)Math.Ceiling(arch(k_eps) / arch(k_W)); // Порядок фильтра
+            Assert.That.Value(N).IsEqual(5);
+
+            var r = N % 2;                              // Нечётность порядка фильтра
+            var dth = Math.PI / N;                      // Угловой шаг между полюсами
+            var beta = arsh(1 / eps_p) / N;
+            Assert.That.Value(beta).IsEqual(0.24518628509618212);
+
+            var sh = Math.Sinh(beta);
+            var ch = Math.Cosh(beta);
+            var poles = new Complex[N];                 // Массив полюсов фильтра
+            if (r != 0) poles[0] = -sh;                 // Если порядок фильтра нечётный, то первым добавляем центральный полюс
+            for (var i = r; i < poles.Length; i += 2)   // Расчёт полюсов
+            {
+                var n = (i - r) / 2 + 1;
+                var th = dth * (n - 0.5);
+
+                var sin = Math.Sin(th);
+                var cos = Math.Cos(th);
+                poles[i] = new Complex(-sh * sin, ch * cos);
+                poles[i + 1] = poles[i].ComplexConjugate;
+            }
+
+            Assert.That.Collection(poles).IsEqualTo(
+                -0.24765029577598821,
+                (-0.076528150056762612, +0.979787021976867),
+                (-0.076528150056762612, -0.979787021976867),
+                (-0.20035329794475673, +0.605541681317744),
+                (-0.20035329794475673, -0.605541681317744)
+                );
+
+            var z_poles = DigitalFilter.ToZArray(poles, dt, Wp);
+
+            Assert.That.Collection(z_poles).IsEqualTo(
+                0.76166135868560936,
+                (0.51881789309466986, +0.78033858156608826),
+                (0.51881789309466986, -0.78033858156608826),
+                (0.65550339923693535, +0.49362618841054584),
+                (0.65550339923693535, -0.49362618841054584)
+            );
+
+            var (A, a_im) = Polynom.Array.GetCoefficientsInverted(z_poles);
+
+            Assert.That.Collection(a_im).ElementsAreEqualTo(0, 2.3e-16);
+            Assert.That.Collection(A).ValuesAreEqual(
+                +1,
+                -3.11030394334882,
+                +4.700669700507134,
+                -4.067694193782959,
+                +2.000259228659479,
+                -0.4503476466802788);
+
+            var G_norm = (r > 0 ? 1 : Gp)
+                / (2.Power(N) / z_poles.Aggregate(Complex.Real, (Z, z) => Z * (1 - z), z => z.Re));
+
+            Assert.That.Value(G_norm).IsEqual(0.0022682232923298762);
+
+            var B = Enumerable.Range(0, N + 1).ToArray(i => G_norm * SpecialFunctions.BinomialCoefficient(N, i));
+
+            Assert.That.Collection(B).ValuesAreEqual(
+                0.0022682232923298762,
+                0.01134111646164938,
+                0.02268223292329876,
+                0.02268223292329876,
+                0.01134111646164938,
+                0.0022682232923298762);
+
+            // Проверяем коэффициенты передачи рассчитанного фильтра
+            var H0 = DoubleArrayDSPExtensions.GetTransmissionCoefficient(A, B, 0).Abs;          // == (r == 1 ? 1 : Gp)
+            var Hp = DoubleArrayDSPExtensions.GetTransmissionCoefficient(A, B, fp / fd).Abs;    // == Gp
+            var Hs = DoubleArrayDSPExtensions.GetTransmissionCoefficient(A, B, fs / fd).Abs;    // <= Gs
+            var Hd = DoubleArrayDSPExtensions.GetTransmissionCoefficient(A, B, 0.5).Abs;        // == 0
+
+            Assert.That.Value(H0).IsEqual(r == 1 ? 1 : Gp, 1.59e-14);
+            Assert.That.Value(Hp).IsEqual(Gp, 9.78e-15);
+            Assert.That.Value(Hs).LessOrEqualsThan(Gs);
+            Assert.That.Value(Hd).IsEqual(0, 5.67e-20);
+        }
+    }
+}
