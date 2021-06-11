f010e1325fce66b5a07dc4665150cc05478555e1
Infarh <shmachilin@yandex.ru>
2021-06-12 02:12:36 +0300

    Алгоритм расчёта коэффициентов полинома ППФ-Баттерворта

---
+++ MathCore.DSP/Filters/ButterworthBandPass.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using MathCore.Annotations;
+
+using static System.Math;
+
+using static MathCore.Polynom.Array;
+using static MathCore.SpecialFunctions;
+// ReSharper disable InconsistentNaming
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+
+namespace MathCore.DSP.Filters
+{
+    public class ButterworthBandPass : ButterworthFilter
+    {
+        private static (double[] A, double[] B) GetPolynoms(Specification Spec, double fmin, double fmax)
+        {
+            var fd = Spec.fd;
+            var Fmin = ToAnalogFrequency(fmin, Spec.dt);
+            var Fmax = ToAnalogFrequency(fmax, Spec.dt);
+
+            var N = (int)Ceiling(Log(Spec.kEps) / Log(Spec.kW));
+            var poles = GetNormPoles(N, Spec.EpsP);
+
+            var ppf_zeros = Enumerable.Repeat(new Complex(), N);
+            var ppf_poles = TransformToBandPassPoles(poles, Fmin, Fmax).ToArray();
+
+            var ppf_zeros_z = Enumerable.Repeat(Complex.ReValue(1), N).AppendLast(Enumerable.Repeat(Complex.ReValue(-1), N)).ToArray();
+            var ppf_poles_z = ToZArray(ppf_poles, Spec.dt);
+
+            var kz0 = Complex.Real;
+            var dW = Consts.pi2 * (Fmax - Fmin);
+            var fd2 = 2 * Spec.fd;
+            for (var i = 0; i < ppf_poles.Length; i += 2)
+            {
+                kz0 *= fd2 * dW;
+                kz0 /= (fd2 - ppf_poles[i]);
+                kz0 /= (fd2 - ppf_poles[i + 1]);
+            }
+            kz0 /= Spec.EpsP;
+
+            var B = GetCoefficientsInverted(ppf_zeros_z).ToArray(v => (v * kz0).Re);
+            var A = GetCoefficientsInverted(ppf_poles_z).ToRe()!;
+
+            return (A, B);
+        }
+
+        public Interval F { get; }
+
+        public ButterworthBandPass(
+            double dt,
+            double fp,
+            double fs,
+            double fmin,
+            double fmax,
+            double Gp = 0.891250938,
+            double Gs = 0.031622777)
+            : this(GetSpecification(dt, fp, fs, Gp, Gs), fmin, fmax)
+        { }
+
+        private ButterworthBandPass(Specification Spec, double fmin, double fmax)
+            : this(GetPolynoms(Spec, fmin, fmax), Spec) => F = (fmin, fmax);
+
+        private ButterworthBandPass((double[] A, double[] B) Polynoms, Specification Spec) : this(Polynoms.B, Polynoms.A, Spec) { }
+
+        private ButterworthBandPass(double[] B, double[] A, Specification Spec) : base(B, A, Spec) { }
+    }
+}
---
+++ Tests/MathCore.DSP.Tests/Filters/ComplexTests/BandPass_fp500_fs1500_df5000.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+using MathCore.DSP.Filters;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MathCore.DSP.Tests.Filters.ComplexTests
+{
+    [TestClass]
+    public class BandPass_fp500_fs1500_df5000 : ComplexTest
+    {
+        private static class Information
+        {
+            /// <summary>Частота дискретизации, Гц</summary>
+            public const double fd = 5000;
+
+            /// <summary>Период дискретизации</summary>
+            public const double dt = 1 / fd;
+
+            /// <summary>Граничная частота полосы пропускания</summary>
+            public const double fp = 10;
+
+            /// <summary>Граничная частота полосы запирания</summary>
+            public const double fs = 40;
+
+            /// <summary>Неравномерность в полосе пропускания (дБ)</summary>
+            public const double Rp = 1;
+
+            /// <summary>Неравномерность в полосе пропускания (дБ)</summary>
+            public const double Rs = 30;
+
+            /// <summary>Нижняя граница полосы пропускания</summary>
+            public const double fmin = 100;
+
+            /// <summary>Верхняя граница полосы пропускания</summary>
+            public const double fmax = 1000;
+
+            public static AnalogBasedFilter.Specification GetSpecification() => new(dt, fp, fs, (-Rp).From_dB(), (-Rs).From_dB());
+        }
+
+        [TestMethod]
+        public void Butterworth()
+        {
+            var filter = new ButterworthBandPass(
+                Information.dt, 
+                Information.fp, Information.fs, 
+                Information.fmin, Information.fmax, 
+                (-Information.Rp).From_dB(), (-Information.Rs).From_dB());
+
+        }
+    }
+}
