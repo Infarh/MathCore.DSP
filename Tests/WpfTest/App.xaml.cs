e5cfff7302cdf2de9793d424478138145c41ceab
Infarh <shmachilin@yandex.ru>
2021-09-07 20:37:54 +0300

    Обновление пакетов

--- Tests/WpfTest/App.xaml.cs
+++ Tests/WpfTest/App.xaml.cs
@@ -1,20 +1,7 @@
-﻿using System.Linq;
-
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-
-namespace WpfTest
+﻿namespace WpfTest
 {
     public partial class App
     {
-        public static ServiceDescriptor[] ServicesCollection { get; private set; }
-        public static bool ServicesInitialized { get; private set; }
-
-        protected override IHostBuilder ConfigureHostBuilderFinal(IHostBuilder builder)
-        {
-            var services = builder.ConfigureServices(s => ServicesCollection = s.ToArray());
-            ServicesInitialized = true;
-            return base.ConfigureHostBuilderFinal(builder);
-        }
+       
     }
 }
