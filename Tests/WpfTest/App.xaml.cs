61a9515c751a74afad53c2af08afb9befaa9636a
Infarh <shmachilin@yandex.ru>
2021-09-07 20:31:21 +0300

    Рефакторинг

--- Tests/WpfTest/App.xaml.cs
+++ Tests/WpfTest/App.xaml.cs
@@ -1,7 +1,20 @@
-﻿namespace WpfTest
+﻿using System.Linq;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WpfTest
 {
     public partial class App
     {
+        public static ServiceDescriptor[] ServicesCollection { get; private set; }
+        public static bool ServicesInitialized { get; private set; }
 
+        protected override IHostBuilder ConfigureHostBuilderFinal(IHostBuilder builder)
+        {
+            var services = builder.ConfigureServices(s => ServicesCollection = s.ToArray());
+            ServicesInitialized = true;
+            return base.ConfigureHostBuilderFinal(builder);
+        }
     }
 }
